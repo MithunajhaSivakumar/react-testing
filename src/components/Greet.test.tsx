8a9ad7137982548dacb0b37ebb2126151493fe6f
MithunajhaS <mithunajhasivakumar2002@gmail.com>
2023-07-09 17:33:26 +0530

    Grouping tests

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -1,14 +1,37 @@
 import { render, screen } from '@testing-library/react';
 import { Greet } from './greet';
 
-test.only('Greet renders correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getByText('Hello')
-    expect(textElement).toBeInTheDocument();
+describe('Greet', ()=> {
+    test('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('Hello')
+        expect(textElement).toBeInTheDocument();
+    })
+    describe('Nested', () => {
+        test('renders correctly 1', () => {
+            render(<Greet />)
+            const textElement = screen.getByText('Hello')
+            expect(textElement).toBeInTheDocument();
+        })
+    })
 })
-
-test.skip('Greet renders with a name', () => {
-    render(<Greet name='Mithu' />)
-    const textElement = screen.getByText('Hello Mithu')
-    expect(textElement).toBeInTheDocument()
+describe('Multiple', () => {
+    test('renders correctly 2', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('Hello')
+        expect(textElement).toBeInTheDocument();
+    })
+})
+describe.skip('Greet : Group name', () => {
+    test('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('Hello')
+        expect(textElement).toBeInTheDocument();
+    })
+    
+    test('renders with a name', () => {
+        render(<Greet name='Mithu' />)
+        const textElement = screen.getByText('Hello Mithu')
+        expect(textElement).toBeInTheDocument()
+    })
 })

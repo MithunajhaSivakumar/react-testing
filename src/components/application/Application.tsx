352d635dc69e6ff577cef98cb57b9aa061e93fb2
MithunajhaS <mithunajhasivakumar2002@gmail.com>
2023-07-09 20:42:24 +0530

    getByRole RTL testing

---
+++ src/components/application/Application.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Application() {
+  return (
+    <form>
+        <div>
+            <label htmlFor='name'>Name</label>
+            <input type='text' id='name'/>
+        </div>
+        <div>
+            <label htmlFor='job-location'>Job Location</label>
+            <select id='job-location'>
+                <option value="">Select a Country</option>
+                <option value="US">United States</option>
+                <option value="GB">United Kingdom</option>
+                <option value="CA">Canada</option>
+                <option value="IN">India</option>
+            </select>
+        </div>
+        <div>
+            <label>
+                <input type='checkbox' id="terms"/> I agree to terms and conditions
+            </label>
+        </div>
+        <button>Submit</button>
+    </form>
+  )
+}

138ea8de1edfeba0b16a4c28e8db84ace515eca1
MithunajhaS <mithunajhasivakumar2002@gmail.com>
2023-07-10 12:44:23 +0530

    getByDisplayValue

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -40,6 +40,9 @@ describe('Application', ()=> {
         const nameElement3 = screen.getByPlaceholderText('Fullname');
         expect(nameElement3).toBeInTheDocument();
 
+        const nameElement4 = screen.getByDisplayValue('Mithunajha');
+        expect(nameElement4).toBeInTheDocument();
+
         const bioElement = screen.getByRole('textbox', {
             name:'Bio',
         });
